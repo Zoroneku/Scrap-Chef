edb37b7477a65066774b9c1c6fe4c1aec04a54e1
Hans Novenson <2889880N@student.gla.ac.uk>
2025-03-07 09:37:17 +0000

    Added urls and basic view function templates

---
+++ scrapchef/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from scrapchef import views
+
+app_name = 'scrapchef'
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    path('login/', views.login, name='login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('feed/', views.feed, name='feed'),
+    path('privacy/', views.privacy, name='privacy'),
+    path('saved/', views.saved, name='saved'),
+    path('post/', views.post, name='post'),
+    path('editpost/', views.editpost, name='editpost'),
+    path('trending/', views.trending, name='trending'),
+    path('bestmeals/', views.bestmeals, name='bestmeals'),
+    path('worstmeals/', views.worstmeals, name='worstmeals'),
+    path('recent/', views.recent, name='recent'),
+    path('rating/', views.rating, name='rating'),
+    path('signout/', views.signout, name='signout'),
+]
--- scrapchef/views.py
+++ scrapchef/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def homepage(request):
+    # add stuff here
+    return 
+
+def login(request):
+    # add stuff here
+    return
+
+def dashboard(request):
+    # add stuff here
+    return 
+
+def feed(request):
+    # add stuff here
+    return
+
+def privacy(request):
+    # add stuff here
+    return 
+
+def saved(request):
+    # add stuff here
+    return
+
+def post(request):
+    # add stuff here
+    return 
+
+def editpost(request):
+    # add stuff here
+    return
+
+def trending(request):
+    # add stuff here
+    return 
+
+def bestmeals(request):
+    # add stuff here
+    return
+
+def worstmeals(request):
+    # add stuff here
+    return 
+
+def recent(request):
+    # add stuff here
+    return
+
+def rating(request):
+    # add stuff here
+    return 
+
+def signout(request):
+    # add stuff here
+    return
