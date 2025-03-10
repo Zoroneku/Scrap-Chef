2f469e992f3a06f41954308c5726de50bef4655f
“Liam <2761753r@student.gla.ac.uk>
2025-03-10 16:20:28 +0000

    User model created

--- scrapchef/models.py
+++ scrapchef/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    UserName = models.CharField(max_length=64)
+    Password = models.CharField(max_length=64)
+    Occupation = models.CharField(max_length=64)
+    ProfilePhoto = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.UserName
