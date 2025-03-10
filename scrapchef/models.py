8dbc84215a84b03b481cacba294974855a5c0deb
“Liam <2761753r@student.gla.ac.uk>
2025-03-10 17:01:04 +0000

    1

--- scrapchef/models.py
+++ scrapchef/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     UserName = models.CharField(max_length=64)
     Password = models.CharField(max_length=64)
     Occupation = models.CharField(max_length=64)
-    ProfilePhoto = models.CharField(max_length=255)
+    ProfilePhoto = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.UserName
