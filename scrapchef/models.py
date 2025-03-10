b24a5f5b6063821ce0987e7f59c20ea2718ce3e5
“Liam <2761753r@student.gla.ac.uk>
2025-03-10 16:32:45 +0000

    Made Basic Models for the entities

--- scrapchef/models.py
+++ scrapchef/models.py
@@ -10,3 +10,29 @@ class User(models.Model):
 
     def __str__(self):
         return self.UserName
+    
+class Post(models.Model):
+    PostID = models.CharField(max_length=64, unique=True)
+    Media = models.CharField(max_length=255)
+    Caption = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.Caption
+    
+class Review(models.Model):
+    ReviewID = models.CharField(max_length=64, unique=True)
+    Taste = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])  
+    Struggle = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])  
+    Preparation = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])  
+
+    def __str__(self):
+        return f"Review {self.ReviewID}"
+    
+class List(models.Model):
+    Title = models.CharField(max_length=64)
+    Description = models.TextField()
+    PostID = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.Title
+
