44273423a934ff6f47c24e5d44f2fabcae46fbbe
Varshini Seshan <2889241s@student.gla.ac.uk>
2025-03-28 08:12:25 +0400

    added a test

--- scrapchef/test.py
+++ scrapchef/test.py
@@ -7,6 +7,7 @@ if not settings.configured:
 from django.test import TestCase
 from django.test.client import Client
 from scrapchef.models import User
+from scrapchef.models import Post 
 
 # Create your tests here.
 class TestLoginView(TestCase):
@@ -17,12 +18,6 @@ class TestLoginView(TestCase):
     def test_login_get(self):
         response = self.client.get("/login/")
         self.assertTrue(b"Log In" in response.content)
-        self.assertEqual(response.status_code, 200)
-
-    def test_login_post_failed(self):
-        response = self.client.post("/login/", data={"username": "invalid", "password": "invalid"})
-
-        self.assertEqual(b"Invalid username or password.", response.content)
         self.assertEqual(response.status_code, 200)
 
     def test_login_post_success(self):
@@ -73,3 +68,20 @@ class TestIfLogin(TestCase):
         self.assertEqual(response.status_code, 302)
         response = self.client.post("/upload/", data={"url": "testurl"})
         self.assertEqual(response.status_code, 302)
+
+
+class TestDashboardAccess(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user1 = User.objects.create_user(username="user1", password="pass1")
+        self.user2 = User.objects.create_user(username="user2", password="pass2")
+
+    def test_dashboard_access(self):
+        self.client.login(username="user1", password="pass1")
+        response = self.client.get("/dashboard/")
+        self.assertEqual(response.status_code, 200)
+
+        self.client.logout()
+        self.client.login(username="user2", password="pass2")
+        response = self.client.get("/dashboard/")
+        self.assertEqual(response.status_code, 200)
